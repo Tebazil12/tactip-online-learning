Fix rotation bug, and mu return type error

tactip_toolkit_dobot/experiments/online_learning/contour_following_2d.py
@@ -201,7 +201,7 @@ class Experiment:
         # frame should still be available
 
         keypoints = common.tap_at(
-            new_location, new_orient, self.robot, self.sensor, meta
+            new_location, np.rad2deg(new_orient), self.robot, self.sensor, meta
         )
 
         self.add_to_alldata(
@@ -378,7 +378,7 @@ def main():
     meta = make_meta()
     ex = Experiment()
 
-    np.set_printoptions(precision=2, suppress=True)
+    # np.set_printoptions(precision=2, suppress=True)
 
     with common.make_robot() as ex.robot, common.make_sensor(meta) as ex.sensor:
         common.init_robot(ex.robot, meta, do_homing=False)
@@ -434,23 +434,29 @@ def main():
                     # set mus to 0 for first line only - elsewhere mu is optimised
                     x_line = dp.add_line_mu(adjusted_disps, 0)
 
+                    # init model (sets hyperpars)
                     model = gplvm.GPLVM(x_line, np.array(new_taps))
-                    print(f"model inited with ls: {str(model.ls)} sigma_f: {str(model.sigma_f)}")
-                    print(f"model data shape: x={np.shape(model.x)}, y={np.shape(model.y)}")
-                    print(model.__dict__)
 
-                    n_lines_str = str(n_lines_in_model).rjust(3, "0")
-                    common.save_data(
-                        model.__dict__, meta, name="gplvm_" + n_lines_str + ".json"
-                    )
-                    n_lines_in_model = n_lines_in_model +1
+
                 else:
                     pass
-                    # todo, optimise mu of line given old data and hyperpars
+                    # optimise mu of line given old data and hyperpars
+                    optm_mu = model.optim_line_mu(adjusted_disps, new_taps)
+
+                    x_line = dp.add_line_mu(adjusted_disps, optm_mu)
+
                     # todo, save line to model (taking care with dimensions...)
 
+                print(f"model inited with ls: {str(model.ls)} sigma_f: {str(model.sigma_f)}")
+                print(f"model data shape: x={np.shape(model.x)}, y={np.shape(model.y)}")
+                print(model.__dict__)
 
-                # todo add new data with adjusted labels to gplvm model
+                n_lines_str = str(n_lines_in_model).rjust(3, "0")
+                common.save_data(
+                    model.__dict__, meta, name="gplvm_" + n_lines_str + ".json"
+                )
+                n_lines_in_model = n_lines_in_model +1
+
 
             # actually add location to list (so as to not repeat self)
             if ex.edge_locations is None:

tactip_toolkit_dobot/experiments/online_learning/offline_setup/gp.py
@@ -72,7 +72,7 @@ def cal_K_star(x, x_star, sigma_f, L, sigma_n):
 
 def calc_covariance(x, x_prime, sigma_f, L):
     if not np.isscalar(sigma_f):
-        raise NameError("sigma_f must be a scalar, not a matrix")
+        raise NameError(f"sigma_f must be a scalar, not an array: {sigma_f} {type(sigma_f)}")
 
     # todo check shape of x and x_prime
 

tactip_toolkit_dobot/experiments/online_learning/offline_setup/gplvm.py
@@ -6,6 +6,7 @@ import tactip_toolkit_dobot.experiments.online_learning.offline_setup.data_proce
 
 class GPLVM:
     sigma_n_y = 1.14  # TODO this is for the normal tactip, needs setting for others!
+    # sigma_n_y = 100
 
     def __init__(self, x, y, sigma_f=None, ls=None):
         """
@@ -35,24 +36,32 @@ class GPLVM:
         return self.max_log_like(sigma_f, l_disp, l_mu, x, y)
 
     def max_ll_optim_mu(self, to_optimise, set_vals):
-        mu = to_optimise
+        [mu] = to_optimise
         disp, y = set_vals
 
-        # make x from disp and optimising mu # same as only one line being passed
-        x = dp.add_mus([disp], mu_limits=[mu, mu])
+        print(f"mu is now {mu}")
 
+        # make x from disp and optimising mu # same as only one line being passed
+        # x = dp.add_mus([disp], mu_limits=[mu, mu])
+
+        # print(f"just before add_line_mu disps is {disp} and mu is {mu}")
         x = dp.add_line_mu(disp, mu)
+
+        # print(x)
 
         # print(y)
         # y = np.array([y])
         # print(y)
-        x = x.reshape(x.shape[0] * x.shape[1], x.shape[2])
+
+        # x = x.reshape(x.shape[0] * x.shape[1], x.shape[2])
         # y = y.reshape(y.shape[0] * y.shape[1], y.shape[2])
 
         # todo add in self.x and self.y otherwise your not using the right model!
 
         all_xs = np.vstack((self.x, x))
         all_ys = np.vstack((self.y, y))
+
+        # print(f"shape of optmising x {np.shape(all_xs)} and y {np.shape(all_ys)}")
 
         return self.max_log_like(self.sigma_f, self.ls[0], self.ls[1], all_xs, all_ys)
 
@@ -66,7 +75,7 @@ class GPLVM:
         # if not np.isscalar(s_cap):
         #     raise NameError("s_cap is not scalar!")
 
-        ls = np.array([l_disp, l_mu])  # TODO confirm 2 ls works with calc_K
+        ls = np.array([l_disp, l_mu])
 
         k_cap = gp.calc_K(x, sigma_f, ls, self.sigma_n_y)
 
@@ -83,7 +92,7 @@ class GPLVM:
         part_3 = -d_cap * 0.5 * np.trace(np.linalg.inv(k_cap) @ s_cap)
 
         neg_val = part_1 + part_2 + part_3
-        # print(neg_val)
+        print(f"max log like is: {-neg_val}")
         return -neg_val  # because trying to find max with a min search
 
     def optim_hyperpars(self, x=None, y=None, start_hyperpars=None, update_data=False):
@@ -156,6 +165,8 @@ class GPLVM:
         # TODO test that hyperpars have been optimised as can't continue without
 
         start_mu = 0  # start mu for line
+        print(f"start mu: {start_mu}")
+
         data = [disp, y]
         # minimizer_kwargs = {"args": data}
         result = scipy.optimize.minimize(

tactip_toolkit_dobot/experiments/online_learning/test_linecollection.py
@@ -46,7 +46,9 @@ def main():
 
         print("Main code...")
 
-        results = ex.collect_line([10, 0], 0, meta)
+        ex.collect_neutral_tap(meta)
+
+        results = ex.collect_line([0, 0], np.deg2rad(30), meta)
 
         print("collected:")
         print(results)